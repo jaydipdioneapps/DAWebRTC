96b49e24e10a500c3bf41398981745ed6fad6485
Jekil Dabhoya <jekil@dioneapps.com>
2025-05-01 12:12:09 +0530

    Fixed package file

--- Package.swift
+++ Package.swift
@@ -7,20 +7,26 @@ let package = Package(
     name: "DAWebRTC",
     platforms: [
         .iOS(.v13)
-    ],
-    products: [
-        .library(name: "DAWebRTC", targets: ["DAWebRTC"])
+    ], products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "DAWebRTC",
+            targets: ["DAWebRTC"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/stasel/WebRTC.git", .upToNextMajor(from: "135.0.0"))
+        //.package(url: "https://github.com/jaydipdioneapps/DACombineAlamofireAPI", from: "1.0.0"),
+            .package(url: "https://github.com/stasel/WebRTC.git", .upToNextMajor(from: "135.0.0"))
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DAWebRTC",
-            dependencies: [
-                .product(name: "WebRTC", package: "WebRTC")
-            ],
-            path: "Sources"
-        )
-    ]
+            dependencies: ["WebRTC"]),
+        .testTarget(
+            name: "DAWebRTCTests",
+            dependencies: ["DAWebRTC"]),
+    ],
+    swiftLanguageModes: [.v5]
+
 )
