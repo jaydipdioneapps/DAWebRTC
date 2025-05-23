7c34437cfc01a36db6d0fc2b64985455c27f60c5
Jekil Dabhoya <jekil@dioneapps.com>
2025-05-23 09:55:54 +0530

    Issue fixed

--- Sources/DAWebRTC/DAWebRTC.swift
+++ Sources/DAWebRTC/DAWebRTC.swift
@@ -168,9 +168,7 @@ public class DAWebRTC: NSObject {
         let videoView = convertViewToRTCMTLVideoView(view: view)
         
         remoteContainerView = convertViewToRTCMTLVideoView(view: remoteView)
-        if let remoteView = remoteContainerView {
-            remoteView.delegate = self
-        }
+        remoteContainerView.delegate = self
         
         if type == .audio {
             self.localAudioTrack = self.peerConnectionFactory.audioTrack(withTrackId: "audio0")
